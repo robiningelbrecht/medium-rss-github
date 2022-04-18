5472bf7f7be46c14a6d50eb6d7064f1b4f8d51e7
robin <robin@baldwin.be>
2022-04-18 12:13:51 +0200

    Init commit

---
+++ api/index.php
@@ -0,0 +1,82 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use App\MediumRssItem;
+use Symfony\Component\HttpFoundation\Request;
+
+$mediumRssItem = MediumRssItem::fromRequest(Request::createFromGlobals());
+
+header('Content-type: image/svg+xml');
+
+echo '
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="800" height="120">
+    <foreignObject width="100%" height="100%">
+        <div xmlns="http://www.w3.org/1999/xhtml">
+            <style>
+                * {
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
+                    font-family: sans-serif
+                }
+
+                .flex {
+                    display: flex;
+                    align-items: center;
+                }
+
+                .container {
+                    height: 120px;
+                    border: 1px solid rgba(0, 0, 0, .2);
+                    padding: 10px 20px;
+                    border-radius: 10px;
+                    background: rgb(255, 255, 255);
+                    background-size: 600% 400%;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                }
+
+                img {
+                    margin-right: 10px;
+                    width: 150px;
+                    height: 100%;
+                    object-fit: cover;
+                }
+
+                .right {
+                    flex: 1;
+                }
+
+                a {
+                    text-decoration: none;
+                    color: inherit
+                }
+
+                p {
+                    line-height: 1.5;
+                    color: #555
+                }
+
+                h3 {
+                    color: #333
+                }
+
+                small {
+                    color: #888;
+                    display: block;
+                    margin-top: 5px;
+                    margin-bottom: 8px
+                }
+            </style>
+            <a class="container flex" href="' . $mediumRssItem->getLink() . '" target="__blank">
+                <img src="' . $mediumRssItem->getImage() . '" />
+                <div class="right">
+                    <h3>' . $mediumRssItem->getTitle() . '</h3>
+                    <small>' . $mediumRssItem->getPubDate()->format('D M Y, H:i') . '</small>
+                    <p>' . $mediumRssItem->getSummary() . '</p>
+                </div>
+            </a>
+        </div>
+    </foreignObject>
+</svg>';
---
+++ src/MediumRssItem.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class MediumRssItem
+{
+    private const RSS_FEED_URL = 'https://medium.com/feed/%s';
+
+    private string $title;
+    private string $link;
+    private \DateTimeImmutable $pubDate;
+    private string $creator;
+    private ?string $image;
+    private string $summary;
+
+    private function __construct(
+        string $userName,
+        int $postIndex,
+    )
+    {
+
+        $content = preg_replace(
+            '/&(?!#?[a-z0-9]+;)/',
+            '&amp;',
+            file_get_contents(sprintf(self::RSS_FEED_URL, $userName))
+        );
+        $feed = new \SimpleXMLElement($content);
+
+        if (empty($feed->channel->item[$postIndex])) {
+            throw new \RuntimeException('Could not fetch post with index ' . $postIndex);
+        }
+
+        $item = $feed->channel->item[$postIndex];
+
+        $this->title = $item->title;
+        $this->link = $item->link;
+        $this->pubDate = \DateTimeImmutable::createFromFormat('D, d M Y H:i:s e', $item->pubDate);
+        $this->creator = $item->children('dc', true)->creator;
+
+        $content = (string)$item->children('http://purl.org/rss/1.0/modules/content/')->encoded;
+        $this->image = $this->extractImageSource($content);
+        $this->summary = $this->extractSummary($content);
+    }
+
+    public function getTitle(): string
+    {
+        return (string)$this->title;
+    }
+
+    public function getLink(): string
+    {
+        return (string)$this->link;
+    }
+
+    public function getPubDate(): \DateTimeImmutable
+    {
+        return $this->pubDate;
+    }
+
+    public function getCreator(): string
+    {
+        return (string)$this->creator;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function getSummary(): string
+    {
+        return $this->summary;
+    }
+
+    public static function fromRequest(Request $request): self
+    {
+        $uriParts = explode(
+            '/',
+            trim($request->getRequestUri(), '/')
+        );
+
+        if (count($uriParts) !== 2) {
+            throw new \RuntimeException('Provide your Medium username and the index of the post. Eg: @robiningelbrecht/0');
+        }
+
+        if (!str_starts_with($uriParts[0], '@')) {
+            throw new \RuntimeException('Make sure your username stats with "@"');
+        }
+
+        return new self($uriParts[0], intval($uriParts[1]));
+    }
+
+    private function extractImageSource(string $content): ?string
+    {
+        $regex = '#src="(.*?)"#';
+        preg_match($regex, $content, $matches);
+
+        if (empty($matches[1])) {
+            return null;
+        }
+
+        $type = pathinfo($matches[1], PATHINFO_EXTENSION);
+        $data = file_get_contents($matches[1]);
+        return 'data:image/' . $type . ';base64,' . base64_encode($data);
+    }
+
+    private function extractSummary($content): ?string
+    {
+        $regex = '#<p>(.*?)</p>#';
+        preg_match($regex, $content, $matches);
+
+        if (empty($matches[1])) {
+            return null;
+        }
+
+        return substr(strip_tags($matches[1]), 0, 60) . ' ...';
+    }
+}

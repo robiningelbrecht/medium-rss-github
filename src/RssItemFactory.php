6652e522eff0f50c78c107b097e128190aff21e8
robin <robin@baldwin.be>
2022-04-23 22:00:18 +0200

    Debug

--- src/RssItemFactory.php
+++ src/RssItemFactory.php
@@ -11,24 +11,25 @@ class RssItemFactory
 
     public static function createMultipleFromRequest(Request $request): array
     {
+        var_dump('1');
         $uriParts = explode(
             '/',
             trim($request->getPathInfo(), '/')
         );
-
+        var_dump('2');
         if (count($uriParts) < 2) {
             throw new \RuntimeException('Provide your Medium username and the index of the post. Eg: @robiningelbrecht/0');
         }
-
+        var_dump('3');
         [$userName, $itemIndexesString] = $uriParts;
-
+        var_dump('4');
         if (!str_starts_with($userName, '@')) {
             throw new \RuntimeException('Make sure your username stats with "@"');
         }
-
+        var_dump('5');
         var_dump('getttt');
         $response = (new Client())->request('GET', sprintf(self::RSS_FEED_URL, $userName));
-        var_dump($response);
+
         if ($response->getStatusCode() != 200) {
             throw new \RuntimeException('Could not fetch your RSS feed. Did you provide a valid username?');
         }
